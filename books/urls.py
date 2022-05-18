2f25b734a9e2dfb85e908bcfab716db1145dcbe1
ÐNikita Kurabtsev <nick3500hunna@gmail.com>
2022-05-18 17:55:37 +0300

    finish views refactoring

--- books/urls.py
+++ books/urls.py
@@ -1,16 +1,10 @@
 from django.urls import path
 
-from books.views import (AuthorCreateApi, AuthorListApi, BookCreateApi, BookDeleteApi,
-                          BookDetailApi, BookListApi, BookUpdateApi, ApiRoot, AuthorListCreateApi)
+from books.views import AuthorListApi, BookListApi, BookDetailApi, ApiRoot
 
 urlpatterns = [
-    path('authors-create/', AuthorCreateApi.as_view()),
-    path('authors-list/', AuthorListApi.as_view(), name='authors-list'),
-    path('books-create/', BookCreateApi.as_view()),
-    path('books-list/', BookListApi.as_view(), name='books-list'),
-    path('books-detail/<int:pk>/', BookDetailApi.as_view()),
-    path('books-delete/<int:pk>/', BookDeleteApi.as_view()),
-    path('books-update/<int:pk>/', BookUpdateApi.as_view()),
+    path('authors/', AuthorListApi.as_view(), name='authors'),
+    path('books/', BookListApi.as_view(), name='books'),
+    path('books/<int:pk>/', BookDetailApi.as_view()),
     path('', ApiRoot.as_view()),
-    path('authors', AuthorListCreateApi.as_view())
 ]
